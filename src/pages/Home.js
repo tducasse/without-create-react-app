92050d7298d4aab6659867f93aaf0fcfefadb94b
Thibaud Ducasse <thibaud.ducasse@gmail.com>
2019-06-02 19:20:12 +1000

    Basic message post

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,5 @@
-import React, { useEffect } from 'react';
-import { Button, Typography, styled } from '@smooth-ui/core-sc';
+import React, { useEffect, useState } from 'react';
+import { Button, Typography, styled, Input } from '@smooth-ui/core-sc';
 import PropTypes from 'prop-types';
 
 const Container = styled.div`
@@ -14,10 +14,37 @@ const Home = (props) => {
     document.title = 'Home';
   }, []);
 
+  const [message, setMessage] = useState('');
+  const [posts, setPosts] = useState([]);
+
+  const post = () => {
+    if (message) {
+      setPosts([message, ...posts]);
+      setMessage('');
+    }
+  };
+
   const { logout } = props;
   return (
     <Container>
       <Typography>Welcome!</Typography>
+      <Input
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+        placeholder="What's on your mind?"
+        onKeyPress={(e) => {
+          if (e.key === 'Enter') {
+            post(message);
+          }
+        }}
+      />
+      <Button onClick={post}>Post</Button>
+      <ul>
+        {posts.map((el, idx) => {
+          // eslint-disable-next-line react/no-array-index-key
+          return <li key={idx}>{el}</li>;
+        })}
+      </ul>
       <Button onClick={logout}>Logout</Button>
     </Container>
   );
